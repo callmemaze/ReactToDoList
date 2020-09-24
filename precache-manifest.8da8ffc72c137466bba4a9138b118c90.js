67110db127bf6372f1b67326ff4f8e9b6f021cb4
DIPESH <dipeshshtha4@gmail.com>
2020-09-24 14:12:43 +0545

    Updates

---
+++ precache-manifest.8da8ffc72c137466bba4a9138b118c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69ff01c369219ea536e18cba3449e3d",
+    "url": "/ReactToDoList/index.html"
+  },
+  {
+    "revision": "c5e966601c93b7444ff9",
+    "url": "/ReactToDoList/static/css/main.860f91e9.chunk.css"
+  },
+  {
+    "revision": "25f0d208b10eae2bdd0c",
+    "url": "/ReactToDoList/static/js/2.ddb88a4e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ReactToDoList/static/js/2.ddb88a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e966601c93b7444ff9",
+    "url": "/ReactToDoList/static/js/main.3ee3bb3f.chunk.js"
+  },
+  {
+    "revision": "824f21ac4a1cbb0ae4ca",
+    "url": "/ReactToDoList/static/js/runtime-main.5647f28f.js"
+  }
+]);
